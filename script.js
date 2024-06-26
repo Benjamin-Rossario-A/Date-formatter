ee31b22981c9333dadc94de3d8780afeb01e9f6e
Benjamin Rossario A <benrobotics74@gmail.com>
2024-06-26 17:36:04 +0530

    Documented JS

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const currentDateParagraph = document.getElementById("current-date");
-const dateOptionsSelectElement = document.getElementById("date-options");
+const currentDateParagraph = document.getElementById("current-date"); //used to display the output to the user.
+const dateOptionsSelectElement = document.getElementById("date-options"); //used to choose format of date.
 
 const date = new Date();
-const day = date.getDate();
+//Date objects encapsulate an integral number that represents milliseconds since the midnight at the beginning of January 1, 1970
+const day = date.getDate(); //used to get the current local date.
 const month = date.getMonth() + 1;
+//.getMonth() is used to get current month. since the index of Jan is 0, we have to add 1 to the result.
 const year = date.getFullYear();
+//The getFullYear() method of Date instances returns the year for this date according to local time.
 const hours = date.getHours();
+//The getHours() method of Date instances returns the hours for this date according to local time.
 const minutes = date.getMinutes();
-
+//The getMinutes() method of Date instances returns the minutes for this date according to local time.
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
@@ -26,3 +30,10 @@ dateOptionsSelectElement.addEventListener("change", () => {
       currentDateParagraph.textContent = formattedDate;
   }
 });
+/*JavaScript Date objects represent a single moment in time in a platform-independent format. Date objects encapsulate an integral number that represents milliseconds since the midnight at the beginning of January 1, 1970, UTC*/
+/*1) .getDate() --> returns the date b/w 1 and 31
+2) .getMonth() --> returns a no. B/w 0 and 11
+                          0 - Jan
+                          11 - Dec
+3) .getFullYear() --> returns a number which represents the year for the provided date.
+4) .getHours() --> returns a number between 0 and 23. This represents the hour for the provided date, where 0 is midnight and 23 is 11 p.m.*/
